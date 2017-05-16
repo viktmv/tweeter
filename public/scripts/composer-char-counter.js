79d9be8ac900802cc16ca8c14d2ee987f66e7b9a
viktmv <viktmv@icloud.com>
2017-05-16 21:49:04 +0000

    add opacity and transition effect

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,8 +3,8 @@ $(function() {
   let tweet = $('.new-tweet textarea')
   let counter = $('.new-tweet .counter')
   tweet.on('input', function() {
-    $('.notice').text('') // clear the notice text
-    
+    $('.notice').css('opacity', '0') // clear the notice text
+
     let length = 140 - this.value.length
     length < 0
       ? counter.css('color', 'red')
