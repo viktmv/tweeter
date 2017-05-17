e2181543a8d99214549d10ee0dce0277dc840190
viktmv <viktmv@icloud.com>
2017-05-17 16:50:22 +0000

    WIP

--- server/index.js
+++ server/index.js
@@ -7,12 +7,26 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
+const {MongoClient} = require('mongodb')
+const MONGODB_URI = 'mongodb://localhost:27017/tweets'
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// const db = require("./lib/in-memory-db");
+let DataHelpers
 
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) return console.error(`Failed to connect: ${MONGODB_URI}`)
+
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  DataHelpers = require("./lib/data-helpers.js")(db)
+  tweetsRoutes = require("./routes/tweets")(DataHelpers)
+  app.use("/tweets", tweetsRoutes);
+  db.close()
+})
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
@@ -20,14 +34,15 @@ const db = require("./lib/in-memory-db");
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+// const DataHelpers = require("./lib/data-helpers.js")(db);
 
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+var tweetsRoutes
+// = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+// app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
